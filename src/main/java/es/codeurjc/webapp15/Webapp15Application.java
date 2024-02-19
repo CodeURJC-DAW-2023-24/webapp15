49e47d1ed4c4a9b0a51962281b60ef0ea9f117d6
csolsona <75030224+csolsona@users.noreply.github.com>
2024-02-19 11:26:42 +0100

    Create Spring Boot project

---
+++ src/main/java/es/codeurjc/webapp15/Webapp15Application.java
@@ -0,0 +1,13 @@
+package es.codeurjc.webapp15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp15Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/codeurjc/webapp15/Webapp15ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.webapp15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
