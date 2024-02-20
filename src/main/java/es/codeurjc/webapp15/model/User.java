d467a63921bc66e54485254f23c8996fc6970850
DiosYisus77 <102817097+DiosYisus77@users.noreply.github.com>
2024-02-20 10:21:02 +0100

    model user entity

---
+++ src/main/java/es/codeurjc/webapp15/model/User.java
@@ -0,0 +1,66 @@
+package es.codeurjc.webapp15.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "UserTable")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+    private String email;
+	private String password;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+
+	public User() {
+	}
+
+	public User(String name, String encodedPassword, String... roles) {
+		this.name = name;
+		this.encodedPassword = encodedPassword;
+		this.roles = List.of(roles);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+
+}
+
