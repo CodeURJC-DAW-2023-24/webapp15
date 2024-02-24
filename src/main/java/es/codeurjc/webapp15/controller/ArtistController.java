80fd940998217eab5de66dabf6f75e32478fd7de
csolsona <75030224+csolsona@users.noreply.github.com>
2024-02-24 20:14:58 +0100

    Artist page now listing items from the database

--- src/main/java/es/codeurjc/webapp15/controller/ArtistController.java
+++ src/main/java/es/codeurjc/webapp15/controller/ArtistController.java
@@ -1,56 +1,57 @@
 package es.codeurjc.webapp15.controller;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 import es.codeurjc.webapp15.model.Artist;
 import es.codeurjc.webapp15.model.Concert;
 import es.codeurjc.webapp15.model.Genre;
+import es.codeurjc.webapp15.repository.ArtistRepository;
+import es.codeurjc.webapp15.repository.ConcertRepository;
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @Controller
 public class ArtistController {
 
+    @Autowired
+    private ArtistRepository artists;
+    @Autowired
+    private ConcertRepository concerts;  
+
     @GetMapping("/artist/{artistName}")
     public String artistController (Model model, @PathVariable String artistName) {
 
-        Artist artist = sampleArtistMap.get(artistName);
-        if (artist == null) {
+        artistName = artistName.toLowerCase().replace('-', ' ');
+        List<Artist> artistsList = artists.findByNameIgnoreCase(artistName);
+
+        if (artistsList.size() == 0) {
             return "error";
         }
 
+        Artist artist = artistsList.getFirst();
+        List<Concert> concertList = concerts.findByArtistName(artist.getName());
         model.addAttribute("artist", artist);
-        model.addAttribute("concerts2", artist.getConcerts());
+        model.addAttribute("concerts", concertList);
         return "info_artist";
     }
 
-    private List<Concert> sampleConcertList = new ArrayList<>() {{
-        add(new Concert(Long.valueOf(123), LocalDateTime.now(),"Vallecas", Integer.valueOf(1000), Float.valueOf(40.5f), "aaaaaaa", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(23), LocalDateTime.now() ,"Madrid", Integer.valueOf(500), Float.valueOf(30), "bbbbbb", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(543), LocalDateTime.now() ,"Barcelona", Integer.valueOf(1000), Float.valueOf(10), "ccccccccc", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(2134), LocalDateTime.now() ,"Villaverde", Integer.valueOf(100), Float.valueOf(90), "dddddddd", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(6788), LocalDateTime.now() ,"Usera", Integer.valueOf(20000), Float.valueOf(12), "eeeeeeeee", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(689), LocalDateTime.now() ,"Carabanchel",Integer.valueOf(1000), Float.valueOf(87), "ffffffff", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(923), LocalDateTime.now() ,"Leganés", Integer.valueOf(2000), Float.valueOf(56), "gggggggggg", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(145), LocalDateTime.now() ,"Móstoles", Integer.valueOf(900), Float.valueOf(23), "hhhhhhhhhh", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(4567), LocalDateTime.now() ,"Fuenlabrada", Integer.valueOf(280), Float.valueOf(55), "iiiiiiiii", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(120), LocalDateTime.now() ,"Getafe", Integer.valueOf(230), Float.valueOf(10), "jjjjjjjjjjj", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(92123), LocalDateTime.now() ,"Alcorcón", Integer.valueOf(110), Float.valueOf(80), "kkkkkkkk", new Artist(), new Genre()));
-        add(new Concert(Long.valueOf(87), LocalDateTime.now() ,"Humanes", Integer.valueOf(100000), Float.valueOf(100), "lllllllll", new Artist(), new Genre()));
-    }};
-
-    private Map<String, Artist> sampleArtistMap = new HashMap<>() {{
-        put("shakira", new Artist(Long.valueOf(1), "Shakira", "ABCDEFGHIJK", false, sampleConcertList));
-        put("taylor-swift", new Artist(Long.valueOf(2), "Taylor Swift", "AAAAAAAAAAAAAAAAAAAAAAAAA", false, sampleConcertList));
-        put("kayne-west", new Artist(Long.valueOf(3), "Kayne West", "BBBBBBBBBBBBBBBBBBBB", false, sampleConcertList));
-    }};
 }
