9583c4db6ab55e2fce8e2c264ff41fccf62d767d
csolsona <75030224+csolsona@users.noreply.github.com>
2024-03-03 13:52:13 +0100

    Filters in search page now working

--- src/main/java/es/codeurjc/webapp15/controller/SearchController.java
+++ src/main/java/es/codeurjc/webapp15/controller/SearchController.java
@@ -1,5 +1,7 @@
 package es.codeurjc.webapp15.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -68,51 +71,18 @@ public class SearchController {
                                               @RequestParam("artists") String[] artists,
                                               @RequestParam("page") int page) {
 
-        for (String a : locations) {
-            a = a.replace("[", "");
-            a = a.replace("]", "");
-            a = a.replace("\"", "");
-            System.out.println(a);
-        }
-        Page<Concert> pageQuery = concerts.findAll(PageRequest.of(page, 6, Sort.by("datetime")));
+        List<String> locationList = formatJSONArrayToList(locations);
+        List<String> artistList = formatJSONArrayToList(artists);
+
+        Pageable pageable = PageRequest.of(page, 6);
+
+        Page<Concert> pageQuery = concertService.findConcerts(pageable, locationList, artistList, null, null, null, null);
+        // Page<Concert> pageQuery = concerts.findAll(PageRequest.of(page, 6, Sort.by("datetime")));
         if (pageQuery.hasContent()) {
 
             Map<String, Object> map = new HashMap<>();
-            StringBuilder htmlBuilder = new StringBuilder();
-
-            for (Concert concert : pageQuery.getContent()) {
-                htmlBuilder.append("<article class=\"event-article\">");
-                htmlBuilder.append("<time>");
-                htmlBuilder.append("<span class=\"day\">" + concert.getDay() + "</span>");
-                htmlBuilder.append("<span class=\"month\">" + concert.getMonth() + "</span>");
-                htmlBuilder.append("</time>");
-                htmlBuilder.append("<div class=\"event-info\">");
-                htmlBuilder.append("<h1><a>" + concert.getArtist().getName() + "</a></h1>");
-                htmlBuilder.append("<p class=\"date-info\">");
-                htmlBuilder.append("<span class=\"weekday\">" + concert.getWeekday() + "</span>");
-                htmlBuilder.append("<span> - </span>");
-                htmlBuilder.append("<span class=\"hour\">" + concert.getHour() + "</span>");
-                htmlBuilder.append("</p>");
-                htmlBuilder.append("<p class=\"venue-info\">");
-                htmlBuilder.append("<span class=\"city\">" + concert.getPlace() + "</span>");
-                htmlBuilder.append("</p>");
-                htmlBuilder.append("</div>");
-                htmlBuilder.append("<button onclick=\"location.href =\'/payment/" + concert.getId().toString() + "\'\">");
-                htmlBuilder.append("<span>Entradas</span>");
-                htmlBuilder.append("<img src=\"/image/point-right.png\" width=\"19px\">");
-                htmlBuilder.append("</button>");
-                if (globalControllerAdvice.globalAdminModel()) {
-                    
-                    htmlBuilder.append("<button class=\"delete-btn\" data-id=\"" + concert.getId() + "\">");
-                    htmlBuilder.append("<span>Eliminar</span>");
-                    htmlBuilder.append("<img src=\"images/point-right.png\" width=\"19px\">");
-                    htmlBuilder.append("</button>");
-                    
-                }
-                htmlBuilder.append("</article>");
-            }
-                    
-            map.put("content", htmlBuilder);
+             
+            map.put("content", htmlBuilder(pageQuery.getContent()));
 
             boolean hasNext = concerts.findAll(PageRequest.of(page+1, 6, Sort.by("datetime"))).hasContent();
             map.put("hasNext", hasNext);
@@ -120,5 +90,63 @@ public class SearchController {
             return ResponseEntity.ok(map);
         }
         return ResponseEntity.noContent().build();
+    }
+
+
+    private List<String> formatJSONArrayToList(String[] arr) {
+
+        List<String> list = new ArrayList<>(Arrays.asList(arr));
+        for (int i = 0; i < list.size(); i++) {
+            String e = list.get(i);
+            e = e.replace("[", "");
+            e = e.replace("]", "");
+            e = e.replace("\"", "");
+            if (e.isEmpty()) {
+                list.remove(i);
+                i--;
+                continue;
+            }
+            list.set(i, e);
+        }
+
+        return list;
+    }
+
+    private String htmlBuilder (List<Concert> concertList) {
+        StringBuilder htmlBuilder = new StringBuilder();
+
+        for (Concert concert : concertList) {
+            htmlBuilder.append("<article class=\"event-article\">");
+            htmlBuilder.append("<time>");
+            htmlBuilder.append("<span class=\"day\">" + concert.getDay() + "</span>");
+            htmlBuilder.append("<span class=\"month\">" + concert.getMonth() + "</span>");
+            htmlBuilder.append("</time>");
+            htmlBuilder.append("<div class=\"event-info\">");
+            htmlBuilder.append("<h1><a>" + concert.getArtist().getName() + "</a></h1>");
+            htmlBuilder.append("<p class=\"date-info\">");
+            htmlBuilder.append("<span class=\"weekday\">" + concert.getWeekday() + "</span>");
+            htmlBuilder.append("<span> - </span>");
+            htmlBuilder.append("<span class=\"hour\">" + concert.getHour() + "</span>");
+            htmlBuilder.append("</p>");
+            htmlBuilder.append("<p class=\"venue-info\">");
+            htmlBuilder.append("<span class=\"city\">" + concert.getPlace() + "</span>");
+            htmlBuilder.append("</p>");
+            htmlBuilder.append("</div>");
+            htmlBuilder.append("<button onclick=\"location.href =\'/payment/" + concert.getId().toString() + "\'\">");
+            htmlBuilder.append("<span>Entradas</span>");
+            htmlBuilder.append("<img src=\"/image/point-right.png\" width=\"19px\">");
+            htmlBuilder.append("</button>");
+            if (globalControllerAdvice.globalAdminModel()) {
+                
+                htmlBuilder.append("<button class=\"delete-btn\" data-id=\"" + concert.getId() + "\">");
+                htmlBuilder.append("<span>Eliminar</span>");
+                htmlBuilder.append("<img src=\"images/point-right.png\" width=\"19px\">");
+                htmlBuilder.append("</button>");
+                
+            }
+            htmlBuilder.append("</article>");
+        }
+
+        return htmlBuilder.toString();
     }
 }
