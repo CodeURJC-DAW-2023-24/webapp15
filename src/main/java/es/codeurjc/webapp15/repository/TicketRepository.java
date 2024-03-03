b512850b64353182fada075ad122aec9b8a07e7a
Javi <54403354+Javarto@users.noreply.github.com>
2024-03-03 16:53:18 +0100

    ticket history on user + download ticket pdf

--- src/main/java/es/codeurjc/webapp15/repository/TicketRepository.java
+++ src/main/java/es/codeurjc/webapp15/repository/TicketRepository.java
@@ -1,8 +1,16 @@
 package es.codeurjc.webapp15.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import es.codeurjc.webapp15.model.Ticket;
 
-public interface TicketRepository extends  JpaRepository<Ticket,Long>{
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
     
+    @Query("select t from Ticket t join fetch t.concert where t.user.id = :userId")
+    Page<Ticket> findByUserId(@Param("userId") Long userId, PageRequest page);
 }
