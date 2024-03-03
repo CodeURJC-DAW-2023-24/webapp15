b217e70135d053b52efad1b31ae4871eaa9a3e39
DiosYisus77 <102817097+DiosYisus77@users.noreply.github.com>
2024-03-03 18:20:38 +0100

    navegacion

--- src/main/java/es/codeurjc/webapp15/repository/TicketRepository.java
+++ src/main/java/es/codeurjc/webapp15/repository/TicketRepository.java
@@ -1,5 +1,7 @@
 package es.codeurjc.webapp15.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,10 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import es.codeurjc.webapp15.model.Ticket;
+import es.codeurjc.webapp15.model.User;
 
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     
     @Query("select t from Ticket t join fetch t.concert where t.user.id = :userId")
     Page<Ticket> findByUserId(@Param("userId") Long userId, PageRequest page);
+
+    List<Ticket> findByUser(User user);
 }
