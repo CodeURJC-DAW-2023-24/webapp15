158b2b520be87643cd9d61b813e3c5e7402a45bd
DiosYisus77 <102817097+DiosYisus77@users.noreply.github.com>
2024-03-03 13:05:30 +0100

    algoritmoSeccionRecomendados

--- src/main/java/es/codeurjc/webapp15/controller/IndexController.java
+++ src/main/java/es/codeurjc/webapp15/controller/IndexController.java
@@ -1,6 +1,8 @@
 package es.codeurjc.webapp15.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,7 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.codeurjc.webapp15.model.Artist;
+import es.codeurjc.webapp15.model.Concert;
+import es.codeurjc.webapp15.model.Genre;
+import es.codeurjc.webapp15.model.Ticket;
+import es.codeurjc.webapp15.model.User;
 import es.codeurjc.webapp15.repository.ArtistRepository;
+import es.codeurjc.webapp15.repository.ConcertRepository;
+import es.codeurjc.webapp15.repository.GenreRepository;
+import es.codeurjc.webapp15.repository.TicketRepository;
+import es.codeurjc.webapp15.service.UserSession;
 
 
 @Controller
@@ -20,15 +30,29 @@ public class IndexController {
 
     @Autowired
     private ArtistRepository artists;
+
+    @Autowired
+    private TicketRepository tickets;
+
+    @Autowired
+    private UserSession session;
     
     @GetMapping("/")
     public String indexController(Model model) {
 
         Page<Artist> artistList = artists.findAll(PageRequest.of(0, 10));
-
+        List<Artist> recommendedArtists;
         Artist mainArtist = artistList.getContent().getFirst();
-        List<Artist> secondaryArtists = artistList.getContent().subList(1, 4);
-        List<Artist> recommendedArtists = artistList.getContent().subList(0, 4);
+        User user = session.getUser();
+        if (user != null){
+            recommendedArtists = getRecomendArtists(user);
+            if (recommendedArtists.isEmpty()){
+                recommendedArtists = artistList.getContent().subList(0, 4);
+            }
+        } else {
+            recommendedArtists = artistList.getContent().subList(0, 4);
+        }
+        List<Artist> secondaryArtists = getRecomendArtists(null);
 
         model.addAttribute("mainArtist", mainArtist);
         model.addAttribute("secondaryArtists", secondaryArtists);
@@ -73,4 +97,17 @@ public class IndexController {
         }
         return ResponseEntity.noContent().build();
     } 
+
+    public List<Artist> getRecomendArtists(User user){
+        List<Ticket> ticket_list = tickets.findByUser(user);
+        Set<Artist> artist_set = new HashSet<>();
+        for (Ticket ticket : ticket_list) {
+            Concert concert = ticket.getConcert();
+            Artist artist = concert.getArtist();
+            artist_set.add(artist);
+        }
+        List<Artist> artist_list;
+        artist_list = List.copyOf(artist_set);
+        return artist_list;
+    }
 }
