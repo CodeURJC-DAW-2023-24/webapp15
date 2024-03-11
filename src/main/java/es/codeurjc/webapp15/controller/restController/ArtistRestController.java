0e307c6c821a3cf2e8cecc97d0bb144ffecd9042
csolsona <75030224+csolsona@users.noreply.github.com>
2024-03-11 13:26:29 +0100

    Add Artist API POST request

--- src/main/java/es/codeurjc/webapp15/controller/restController/ArtistRestController.java
+++ src/main/java/es/codeurjc/webapp15/controller/restController/ArtistRestController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import es.codeurjc.webapp15.model.Artist;
 import es.codeurjc.webapp15.service.ArtistService;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+
 
 
 
@@ -50,8 +57,23 @@ public class ArtistRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         
         return new ResponseEntity<>(artist.get(), HttpStatus.OK);
-        
+
     }
+
+    @PostMapping("")
+    public ResponseEntity<Artist> createArtist(@RequestBody Artist artist) {
+        try {
+            artistService.save(artist);
+            // Location doesn't return the expected value
+            URI location = fromCurrentRequest().path("artists/{id}").buildAndExpand(artist.getId()).toUri();
+            return ResponseEntity.created(location).body(artist);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+    
     
     
     
