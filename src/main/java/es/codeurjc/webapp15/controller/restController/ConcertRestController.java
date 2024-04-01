e4414193193c5bcb8fea40a29397ee7f985401f2
DiosYisus77 <102817097+DiosYisus77@users.noreply.github.com>
2024-04-01 14:30:14 +0200

    ApiResponses concert

--- src/main/java/es/codeurjc/webapp15/controller/restController/ConcertRestController.java
+++ src/main/java/es/codeurjc/webapp15/controller/restController/ConcertRestController.java
@@ -19,14 +19,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import es.codeurjc.webapp15.model.Artist;
 import es.codeurjc.webapp15.model.Concert;
 
 import es.codeurjc.webapp15.service.ConcertService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,9 +49,28 @@ public class ConcertRestController {
     private ConcertService concertService;
 
     // View search concerts
+    @Operation(summary = "Get a page of concerts")
+    @ApiResponses(value = {
+    @ApiResponse(
+    responseCode = "200",
+    description = "Found the page",
+    content = {@Content(
+    mediaType = "application/json",
+    schema = @Schema(implementation=Concert.class)
+    )}
+    ),
+    @ApiResponse(
+    responseCode = "400",
+    description = "Invalid id supplied",
+    content = @Content
+    ),
+    @ApiResponse(
+    responseCode = "404",
+    description = "Page not found",
+    content = @Content
+    )
+    })
     @GetMapping("")
-    @Operation(summary = "Search concerts")
-    @ApiResponse(responseCode = "200", description = "Concerts retrieved")
     public ResponseEntity<List<Concert>> getConcerts(@RequestParam(value = "page", defaultValue = "0") int page,
                                                         @RequestParam(value = "locations", defaultValue = "") String[] locations,
                                                         @RequestParam(value = "artists", defaultValue = "") String[] artists) {
@@ -65,6 +87,27 @@ public class ConcertRestController {
         return new ResponseEntity<>(concerts.getContent(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a concert by id")
+    @ApiResponses(value = {
+    @ApiResponse(
+    responseCode = "200",
+    description = "Found the page",
+    content = {@Content(
+    mediaType = "application/json",
+    schema = @Schema(implementation=Concert.class)
+    )}
+    ),
+    @ApiResponse(
+    responseCode = "400",
+    description = "Invalid id supplied",
+    content = @Content
+    ),
+    @ApiResponse(
+    responseCode = "404",
+    description = "Concert not found",
+    content = @Content
+    )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Object> getConcert(@PathVariable int id) {
         
@@ -83,10 +126,27 @@ public class ConcertRestController {
     }
 
     @PostMapping("")
-    @Operation(summary = "Create a new concert")
-    @ApiResponse(responseCode = "201", description = "Concert created")
-    @ApiResponse(responseCode = "400", description = "Concert not created")
-    @ApiResponse(responseCode = "403", description = "User not authorized")
+    @Operation(summary = "Create a concert")
+    @ApiResponses(value = {
+    @ApiResponse(
+    responseCode = "201",
+    description = "Concert created corectly",
+    content = {@Content(
+    mediaType = "application/json",
+    schema = @Schema(implementation=Concert.class)
+    )}
+    ),
+    @ApiResponse(
+    responseCode = "400",
+    description = "Concert not created",
+    content = @Content
+    ),
+    @ApiResponse(
+    responseCode = "403",
+    description = "User not authorized",
+    content = @Content
+    )
+    })
     public ResponseEntity<Concert> createConcert(@RequestBody Concert concert) throws IOException, SQLException {
         if((concert.getArtist() == null) || (concert.getGenre()==null) || (concert.getDatetime()==null)|| (concert.getNum_tickets()==null)|| (concert.getPlace()==null)|| (concert.getPrice()==null)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -96,6 +156,27 @@ public class ConcertRestController {
         
     }
     
+    @Operation(summary = "Delete a concert by id")
+    @ApiResponses(value = {
+    @ApiResponse(
+    responseCode = "200",
+    description = "Concert created corectly",
+    content = {@Content(
+    mediaType = "application/json",
+    schema = @Schema(implementation=Concert.class)
+    )}
+    ),
+    @ApiResponse(
+    responseCode = "400",
+    description = "Concert not deleted",
+    content = @Content
+    ),
+    @ApiResponse(
+    responseCode = "403",
+    description = "User not authorized",
+    content = @Content
+    )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Concert> deleteConcert(@PathVariable long id) {
         Concert concert = concertService.findById(id).get();
