4d036e81872067ec1350b835d7b74b1bc5904f80
csolsona <75030224+csolsona@users.noreply.github.com>
2024-04-23 13:15:36 +0200

    Add workaround to check if page is last

--- frontend/src/app/components/search/search.component.ts
+++ frontend/src/app/components/search/search.component.ts
@@ -1,11 +1,9 @@
 import { Component } from "@angular/core";
 import { SearchService } from "../../services/search.service";
-import { LoginService } from "../../services/login.service";
-import { NgbNavModule, NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
 import { Concert } from "../../models/concert.model";
 import { SearchParamsFields } from "../../utils/search-params";
-import { Observable, catchError, firstValueFrom, map } from 'rxjs';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 import { SpringResponse } from "../../utils/spring-response";
 import { Artist } from "../../models/artist.model";
 
@@ -18,14 +16,20 @@ export class SearchComponent {
     query?: SpringResponse<Concert[]>;
     concerts: Concert[] = [];
     artists: Artist[] = [];
-    locations: String[] = [];
+    locations: string[] = [];
 
-    constructor(private searchService: SearchService, private loginService: LoginService) { }
+    currentPage: number = 0;
+    last: boolean = false;
+
+    private checkedArtists: string[] = [];
+    private checkedLocations: string[] = [];
+
+    constructor(private searchService: SearchService) { }
 
     ngOnInit() {
-        this.searchConcerts();
-        this.getArtists();
-        this.getLocations();
+        this.searchConcerts(true);
+        this.getArtistsList();
+        this.getLocationsList();
     }
 
     private searchPage<T>(relativeUrl: string, params?: SearchParamsFields): Observable<SpringResponse<T>> {
@@ -36,7 +40,7 @@ export class SearchComponent {
                     return response;
                 }),
                 catchError((error: HttpErrorResponse) => {
-                    throw error;
+                    return throwError(() => error);
                 })
             )
     }
@@ -54,32 +58,78 @@ export class SearchComponent {
             )
     }
 
-    searchConcerts(params?: SearchParamsFields): void {
-        this.searchPage<Concert[]>("/concerts", params ?? {})
+    searchConcerts(clearSearch: boolean): void {
+        if (clearSearch) {this.concerts = [] }
+        clearSearch
+            ? this.currentPage = 0
+            : this.currentPage++;
+
+        this.searchPage<Concert[]>("/concerts", this.getParams(this.currentPage))
             .subscribe((response: SpringResponse<Concert[]>) => {
                 this.query = response;
                 this.concerts.push(...this.convertDatetime(response.content));
             })
+
+        // This should be included in SpringResponse object, but it doesn't seem to work well
+        // so, sketchy workaround for now.
+        this.isLast().subscribe((response: boolean) => {
+            this.last = response;
+        })
     }
 
-    private convertDatetime(concerts: Concert[]) {
+    private isLast(): Observable<boolean> {
+        return this.searchPage<Concert[]>("/concerts", this.getParams(this.currentPage + 1))
+            .pipe(
+                map(() => false),
+                catchError((error: HttpErrorResponse) => {
+                    return of(error.status === 404 ? true : false)
+                })
+            )
+    }
+
+    private convertDatetime(concerts: Concert[]): Concert[] {
         concerts.forEach((concert) => {
             concert.datetime = new Date(concert.datetime);
         })
         return concerts;
     }
 
-    getArtists(): void {
+    private getArtistsList(): void {
         this.search<Artist[]>("/concerts/artists")
             .subscribe((response: Artist[]) => {
                 this.artists = response;
             })
     }
 
-    getLocations(): void {
-        this.search<String[]>("/concerts/locations")
-            .subscribe((response: String[]) => {
+    private getLocationsList(): void {
+        this.search<string[]>("/concerts/locations")
+            .subscribe((response: string[]) => {
                 this.locations = response;
             })
+    }
+
+    updateCheckedArtists(artistName: string): void {
+        this.checkedArtists = this.updateCheckedList(this.checkedArtists, artistName);
+        this.searchConcerts(true);
+    }
+
+    updateCheckedLocations(location: string): void {
+        this.checkedLocations = this.updateCheckedList(this.checkedLocations, location);
+        this.searchConcerts(true);
+    }
+
+    private updateCheckedList(list: string[], value: string): string[] {
+        return list.includes(value)
+            ? list.filter((item) => item !== value)
+            : list.concat(value);
+    }
+
+    private getParams(page: number): SearchParamsFields {
+        return {
+            page: page,
+            size: 6,
+            artists: this.checkedArtists,
+            locations: this.checkedLocations,
+        }
     }
 }
